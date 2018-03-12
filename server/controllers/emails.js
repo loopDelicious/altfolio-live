87f03e9e8457b7bc4be2db0b824a3cd114c67694
Joyce Lin <joycelin79@gmail.com>
2018-03-11 19:57:25 -0700

    complete routes for Emails table

--- server/controllers/emails.js
+++ server/controllers/emails.js
@@ -16,4 +16,64 @@ module.exports = {
             .then(emails => res.status(200).send(emails))
             .catch(error => res.status(400).send(error));
     },
+
+    retrieve(req, res) {
+        return Email
+            .findById(req.params.email)
+            .then(email => {
+                if(!email) {
+                    return res.status(404).send({
+                        message: 'Email not found.',
+                    });
+                }
+                return res.status(200).send(email);
+            })
+            .catch(error => res.status(400).send(error));
+    },
+
+    update(req, res) {
+        return Email
+            .find({
+                where: {
+                    email: req.params.email
+                },
+            })
+            .then(email => {
+                if (!email) {
+                    return res.status(404).send({
+                        message: 'Email not found.',
+                    });
+                }
+                return email
+                    .update({
+                        portfolio: req.body.portfolio || email.portfolio,
+                    })
+                    .then(() => res.status(200).send(email))
+                    .catch((error) => res.status(400).send(error));
+            })
+            .catch((error) => res.status(400).send(error));
+    },
+
+    destroy(req, res) {
+        return Email
+            .find({
+                where: {
+                    email: req.params.email
+                },
+            })
+            .then(email => {
+                if (!email) {
+                    return res.status(404).send({
+                        message: 'Email not found.',
+                    });
+                }
+                return email
+                    .destroy()
+                    .then(() => res.status(200).send({
+                        message: "Email deleted successfully."
+                    }))
+                    .catch((error) => res.status(400).send(error));
+            })
+            .catch((error) => res.status(400).send(error));
+    }
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,21 @@
 const emailsController = require('../controllers').emails;
 
 module.exports = (app) => {
+
     app.get('/api', (req, res) => res.status(200).send({
         message: 'Welcome to the Emails API',
     }));
 
     app.post('/api/emails', emailsController.create);
     app.get('/api/emails', emailsController.list);
+    app.get('/api/emails/:email', emailsController.retrieve);
+    app.put('/api/emails/:email', emailsController.update);
+    app.delete('/api/emails/:email', emailsController.destroy);
+
+    app.all('/api/todos/:email', (req, res) => {
+        res.status(405).send({
+            message: "Method not allowed"
+        })
+    })
 
 };
